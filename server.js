4fc42cc08448be6130efe45d0491193fdd046b7a
deliacodes <deliacodes@gmail.com>
2018-01-15 20:42:34 -0800

    Adds comments roughing out the requirements for the project.

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const router = express.Router();
 
 const app = express();
 
+//modularize routes
+
+//blog posts for the server
+
+//get the blog posts that are on the server
 app.get('/blog-posts', (req, res) => {
 
 })
+
+//post to the blog posts on the server
+
+//delete a blog post
+//update a blog post using put
