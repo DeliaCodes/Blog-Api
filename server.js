cf005432a6cf3cc05266c9f6d2e90dbb78251c82
deliacodes <deliacodes@gmail.com>
2018-01-15 21:37:48 -0800

    Creates blog post on server run. Successfully gets and deletes blog posts.

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const router = express.Router();
 const app = express();
 
 //modularize routes
-const BlogPosts = require('./models');
+const {
+  BlogPosts
+} = require('./models');
 
 //blog posts for the server
+BlogPosts.create('Star Wars and the use of Stock Characters', 'Is Lana Del Ray really Rey? Who Can tell the difference? What were those small furry creatures anyway? How do we know snoke isnt secret alternative universe Darth Maul?', 'by Seru4ever');
 
 //get the blog posts that are on the server
 app.get('/blog-posts', (req, res) => {
@@ -19,6 +22,7 @@ app.get('/blog-posts', (req, res) => {
 //delete a blog post
 app.delete('/blog-posts/:id', (req, res) => {
   BlogPosts.delete(req.params.id);
+  res.send(204).end();
 })
 
 //update a blog post using put
