7c61ec1cf3f24f0f3d291616e6e3340bac0703fe
deliacodes <deliacodes@gmail.com>
2018-01-31 22:03:32 -0800

    Adds a GET integration test.

--- test/test-server.js
+++ test/test-server.js
@@ -22,10 +22,34 @@ describe('Blog post', function () {
 
   it('list posts on GET', function () {
 
+    return chaiHttp.request(app)
+      .get('/blog-posts')
+      .then(function (res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
+        const expectedKeys = ['title', 'content', 'author'];
+        res.body.forEach(function (item) {
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
   });
 
-
   it('add a Blog post on POST', function () {
+
+    const newPost = {
+      title: 'Where Have All The Pigs Gone?',
+      content: 'They have caught the flu.',
+      author: 'Miss Piggy'
+    };
+
+    return chaiHttp.request(app)
+      .post('/blog-posts')
+      .send(newPost)
+      .then(function (res) {
+
+      });
 
   });
 
