e7a19406ff9cbccc565157a4881b09ed396b37ad
deliacodes <deliacodes@gmail.com>
2018-02-11 11:45:02 -0800

    Roughed out the model to use mongoose.

--- models.js
+++ models.js
@@ -1,22 +1,34 @@
 const uuid = require('uuid');
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
+const mongoose = require('mongoose');
 
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
+const blogPostSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
+});
 
 
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
+
+
+
+//I don't think this is needed.
+/* function StorageException(message) {
+  this.message = message;
+  this.name = "StorageException";
 }
 
 const BlogPosts = {
-  create: function(title, content, author, publishDate) {
+  create: function (title, content, author, publishDate) {
     const post = {
       id: uuid.v4(),
       title: title,
@@ -27,7 +39,7 @@ const BlogPosts = {
     this.posts.push(post);
     return post;
   },
-  get: function(id=null) {
+  get: function (id = null) {
     // if id passed in, retrieve single post,
     // otherwise send all posts.
     if (id !== null) {
@@ -35,19 +47,21 @@ const BlogPosts = {
     }
     // return posts sorted (descending) by
     // publish date
-    return this.posts.sort(function(a, b) {
+    return this.posts.sort(function (a, b) {
       return b.publishDate - a.publishDate
     });
   },
-  delete: function(id) {
+  delete: function (id) {
     const postIndex = this.posts.findIndex(
       post => post.id === id);
     if (postIndex > -1) {
       this.posts.splice(postIndex, 1);
     }
   },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
+  update: function (updatedPost) {
+    const {
+      id
+    } = updatedPost;
     const postIndex = this.posts.findIndex(
       post => post.id === updatedPost.id);
     if (postIndex === -1) {
@@ -64,7 +78,10 @@ function createBlogPostsModel() {
   const storage = Object.create(BlogPosts);
   storage.posts = [];
   return storage;
-}
+} */
 
+const BlogPosts = mongoose.model('BlogPosts', blogPostSchema);
 
-module.exports = {BlogPosts: createBlogPostsModel()};
+module.exports = {
+  BlogPosts
+};
